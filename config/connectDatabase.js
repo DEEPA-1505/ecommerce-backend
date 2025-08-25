3bbbd8b0bdeaab85aec08cc0d75d9b4727ae2980
DEEPA_1505 <deepikar4229@gmail.com>
2025-08-25 15:11:58 +0530

    updated

--- config/connectDatabase.js
+++ config/connectDatabase.js
@@ -1,12 +1,29 @@
+// const mongoose = require('mongoose');
+
+
+
+// const connectDatabase = () => {
+//     mongoose.connect(process.env.DB_URL).then((con) => {
+//         console.log('MongoDB connected to host: '+con.connection.host)
+//     })
+
+// };
+
+// module.exports = connectDatabase;
+
 const mongoose = require('mongoose');
 
-
-
-const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URL).then((con) => {
-        console.log('MongoDB connected to host: '+con.connection.host)
-    })
-
+const connectDatabase = async () => {
+  try {
+    const con = await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB connected to host: ' + con.connection.host);
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDatabase;
